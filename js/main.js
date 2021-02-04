44399e092bba5d5cb2cbedc9fc31f8391c1a8f2a
askomarov <askomarov13@mail.ru>
2021-02-04 22:33:03 +0300

    #ДЗ №2 добавил функции создания массива из 10 сгенерированных JS-объектов

--- js/main.js
+++ js/main.js
@@ -1,9 +1,120 @@
+'use strict';
+
 const getRandomInteger = function (min, max) {
+  if (min < 0 || max < 0) {
+    return alert('диапазон может быть только положительный, включая ноль.')
+  } else if (max < min) {
+    return Math.round(Math.random() * (min - max)) + max;
+  } else if (min === max) {
+    return min;
+  }
   return Math.round(Math.random() * (max - min)) + min;
-}
-getRandomInteger();
+};
+getRandomInteger(1, 1);
+
 
 const getRandomFloat = function (min, max, NUMBER_ORDER) {
+  if (min < 0 || max < 0) {
+    return alert('диапазон может быть только положительный, включая ноль.')
+  } else if (max < min) {
+    return ((Math.random() * (min - max)) + max).toFixed(NUMBER_ORDER);
+  } else if (min === max) {
+    return min.toFixed(NUMBER_ORDER);
+  }
   return (Math.random() * (max - min) + min).toFixed(NUMBER_ORDER);
-}
-getRandomFloat();
+
+};
+getRandomFloat(2, 2, 2);
+////////////////////////////////////////////////////////////////////////////////
+// значение кол-ва генерированных объектов
+const SIMILAR_OBJECT_COUNT = 10;
+
+// данные для объекта недвижимости
+const HOUSE_TITLE = [
+  'супер предложение',
+  'ограниченное предложение',
+  'специально для Вас',
+  'лучшее предложение в данной категории',
+];
+const HOUSE_TYPE = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+];
+const CHECKINS_TIMES = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const CHECKOUT_TIMES = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const ROOM_FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+const HOUSE_DESC = [
+  'интерьев выполнен в минималистичном стиле',
+  'можно с животными',
+  'хозяин - гид проводит экскурсии',
+  'ближайший магазинчик лучший в районе по морепродуктам',
+];
+const HOUSE_PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+const MIN_X = 35.65;
+const MAX_X = 35.70;
+const MIN_Y = 139.70;
+const MAX_Y = 139.80;
+const LOCATION_NUM_ORDER = 5;
+
+//  функция получения случайного элемента из массива
+const getRandomArrayElement = (elemets) => {
+  return elemets[getRandomInteger(0, elemets.length - 1)];
+};
+
+//  функция получения массива случайной длинны из массива
+const getRandomLengthArray = (elemets) => {
+  return elemets.splice(0, elemets.length - getRandomInteger(0, elemets.length));
+};
+// функция генерации случайного номера в название аватара
+const randomNumberImage = '0' + getRandomInteger(1,8);
+
+
+const mainObject = () => {
+  const result = [
+    {
+      avatar: 'img/avatars/user' + randomNumberImage + '.png',
+    },
+    {
+      title: getRandomArrayElement(HOUSE_TITLE),
+      address: '{{location.x}} , {{location.y}}',
+      price: getRandomInteger(1,100000),
+      type: getRandomArrayElement(HOUSE_TYPE),
+      rooms:getRandomInteger(1,10),
+      guests:getRandomInteger(1,10),
+      checkin:getRandomArrayElement(CHECKINS_TIMES),
+      checkout:getRandomArrayElement(CHECKOUT_TIMES),
+      description:getRandomArrayElement(HOUSE_DESC),
+      features: getRandomLengthArray(ROOM_FEATURES),
+      photos:getRandomLengthArray(HOUSE_PHOTOS),
+    },
+    {
+      x: getRandomFloat(MIN_X, MAX_X, LOCATION_NUM_ORDER),
+      y: getRandomFloat(MIN_Y, MAX_Y, LOCATION_NUM_ORDER),
+    }]
+  return result;
+};
+//получаем массив объектов - адрессов с координатами
+const mainObjectList = new Array(SIMILAR_OBJECT_COUNT).fill(null).map(() => mainObject());
+// console.log(mainObjectList);
+mainObjectList;
