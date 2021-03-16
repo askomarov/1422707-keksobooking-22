2670d810ef01f13fa42523e228de6f4b862475d6
askomarov <askomarov13@mail.ru>
2021-03-16 22:49:39 +0300

    переписал фильтр меток на карте

--- js/filter-map-form.js
+++ js/filter-map-form.js
@@ -1,88 +1,78 @@
 /* global _:readonly */
 import { createPointsOnMap, deleteMarker } from './map.js';
 
+const RERENDER_DELAY = 500;
+const PRICE_LOW = 10000;
+const PRICE_HIGH = 50000;
+
 const mapFiltersForm = document.querySelector('.map__filters');
 const houseTypeFilter = mapFiltersForm.querySelector('#housing-type');
 const housePriceFilter = mapFiltersForm.querySelector('#housing-price');
 const houseRoomsFilter = mapFiltersForm.querySelector('#housing-rooms');
 const houseGuestsFilter = mapFiltersForm.querySelector('#housing-guests');
 
+const filterByType = (item) => {
+  if (houseTypeFilter.value === 'any' || item.offer.type === houseTypeFilter.value) {
+    return true
+  } false;
+};
 
-const FILTERED_OFFER_QUANTITY = 10;
-// не чаще, чем раз в полсекунды (устранение дребезга).
-const RERENDER_DELAY = 500;
-const PRICE_LOW = 10000;
-const PRICE_HIGH = 50000;
+const filterByPrice = (item) => {
+  if (housePriceFilter.value === 'any') {
+    return true
+  } else if (housePriceFilter.value === 'middle') {
+    return item.offer.price >= PRICE_LOW && item.offer.price < PRICE_HIGH
+  } else if (housePriceFilter.value === 'low') {
+    return item.offer.price < PRICE_LOW
+  } else if (housePriceFilter.value === 'high') {
+    return item.offer.price >= PRICE_HIGH
+  } return false
+};
 
-
-const featuresFilter = (item) => {
+const filterByFeatures = (item) => {
   const checkedFeatures = mapFiltersForm.querySelectorAll('.map__checkbox:checked');
-  let i = 0;
-  checkedFeatures.forEach((feature) => {
-    if (item.offer.features.includes(feature.value))
-      i++;
-  });
-  return i === checkedFeatures.length;
-};
-
-const typeFilter = (item) => {
-  return houseTypeFilter.value === 'any' || item.offer.type === houseTypeFilter.value;
-};
-
-const roomsFilter = (item) => {
-  return houseRoomsFilter.value === 'any' || item.offer.rooms === Number(houseRoomsFilter.value);
-};
-
-const capacityFilter = (item) => {
-  return houseGuestsFilter.value === 'any' || item.offer.guests === Number(houseGuestsFilter.value);
-};
-
-const priceFilter = (item) => {
-  const filterPrice = housePriceFilter.value;
-  const itemPrice = item.offer.price;
-
-  switch (filterPrice) {
-    case 'middle':
-      return itemPrice >= PRICE_LOW && itemPrice < PRICE_HIGH;
-    case 'low':
-      return itemPrice < PRICE_LOW;
-    case 'high':
-      return itemPrice >= PRICE_HIGH;
-    default:
-      return true;
+  if (checkedFeatures.length === 0) {
+    return true
+  } else {
+    let intemIncludeFeature = 0;
+    checkedFeatures.forEach((feature) => {
+      if (item.offer.features.includes(feature.value)) {
+        intemIncludeFeature++;
+      } false
+    });
+    if (intemIncludeFeature === checkedFeatures.length) {
+      return true
+    } false
   }
+}
+
+const filterByRooms = (item) => {
+  if (houseRoomsFilter.value === 'any' || item.offer.rooms === Number(houseRoomsFilter.value)) {
+    return true
+  } false
 };
 
-const isItemMatched = (item) => {
-  if (featuresFilter(item) && roomsFilter(item) && capacityFilter(item) &&
-    priceFilter(item) && typeFilter(item)) {
-    return true;
-  }
+const filterByGuests = (item) => {
+  if (houseGuestsFilter.value === 'any' || item.offer.guests === Number(houseGuestsFilter.value)) {
+    return true
+  } false
 };
 
-const getFilteredItems = (array) => {
-  const filteredItems = [];
-  for (let i = 0; i < array.length; i++) {
-    const item = array[i];
-    if (isItemMatched(item)) {
-      filteredItems.push(item);
-    }
-    if (filteredItems.length === FILTERED_OFFER_QUANTITY) {
-      return filteredItems;
-    }
-  }
-  return filteredItems;
-};
-
-const onFilterChange = (array) => {
-  const filteredItems = getFilteredItems(array);
+const renderFilteredArray = (array) => {
+  const filteredArray = array
+    .filter(filterByType)
+    .filter(filterByPrice)
+    .filter(filterByRooms)
+    .filter(filterByGuests)
+    .filter(filterByFeatures);
   deleteMarker();
-  createPointsOnMap(filteredItems);
+  createPointsOnMap(filteredArray);
 };
 
-const turnOnFilterListener = (array) => {
-  mapFiltersForm.addEventListener('change', _.debounce(() => onFilterChange(array), RERENDER_DELAY,
+const filterMapForm = (array) => {
+  mapFiltersForm.addEventListener('change', _.debounce(() =>
+    renderFilteredArray(array), RERENDER_DELAY,
   ));
 };
 
-export { turnOnFilterListener }
+export { filterMapForm };
--- js/main.js
+++ js/main.js
@@ -2,21 +2,17 @@ import { showAlert } from './util.js';
 import { setInputMinPrice, roomsSelectListener, syncCheckTime, makeFormsDisabled, makeMapFormsActive, submitForm, showSuccessPopupMessage, showErrorPopupMessage, onBtnResetAddFormListener } from './form.js';
 import { loadMap, createPointsOnMap } from './map.js';
 import { getData } from './get-send-data.js';
-import { turnOnFilterListener } from './filter-map-form.js';
+import { filterMapForm } from './filter-map-form.js';
 import { previewAvatar, previewBackground } from './preview-avatar-background.js';
-
-const OFFER_QUANTITY = 10;
 
 const sendURL = 'https://22.javascript.pages.academy/keksobooking';
 const getURL = 'https://22.javascript.pages.academy/keksobooking/data';
 
-
-
 makeFormsDisabled();
 
 const getDataSuccess = (data) => {
-  createPointsOnMap(data.slice(0, OFFER_QUANTITY))
-  turnOnFilterListener(data)
+  createPointsOnMap(data)
+  filterMapForm(data)
   makeMapFormsActive();
 };
 
